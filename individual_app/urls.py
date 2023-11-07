9c78a55b17dcef9f185e4588ff9c9350af7050dd
Harrisonray73@uccs.edu <107588181+Harrisonray73@users.noreply.github.com>
2023-11-07 16:04:17 -0700

    commit03

--- individual_app/urls.py
+++ individual_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpattern =[
-    # difines the url pattern '' is empty represents
-    # the path to app views, index is the function 
-    # defined in view.py name='index' is dynamic.
-    path('', views.index, name='index'),
+urlpatterns = [
+#path function defines a url pattern
+#'' is empty to represent based path to app
+# views.index is the function defined in views.py
+# name='index' parameter is to dynamically create url
+# example in html <a href="{% url 'index' %}">Home</a>.
+path('', views.index, name='index'),
 ]
