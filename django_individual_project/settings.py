dca0c4fa60b05c80b8e8db0ede47b1fb46956356
Harrisonray73@uccs.edu <107588181+Harrisonray73@users.noreply.github.com>
2023-11-07 14:55:09 -0700

    second commit

--- django_individual_project/settings.py
+++ django_individual_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'individual_project',
+    # Adding the name of my application here
+    'individual_app',
 ]
 
 # Add support for authenticating users
