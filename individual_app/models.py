420c769d5a39e94d5e6e4f90a2bcc8d769bef575
Harrisonray73@uccs.edu <107588181+Harrisonray73@users.noreply.github.com>
2023-12-10 20:33:58 -0700

    update7

--- individual_app/models.py
+++ individual_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+
+#List of choices for major value in database, human readable name
+    TITLE = (
+('PRES', 'President of the PTA'),
+('VICE', 'Vice President of the PTA'),
+('SECY', 'Secretary of the PTA'),
+('TRES', 'Treasure of the PTA'),
+('MEM', 'Member of the PTA'),
+)
+
+    name = models.CharField(max_length=200)
+    email = models.CharField("USER Email", max_length=200)
+    title = models.CharField(max_length=200, choices=TITLE, blank = True)
