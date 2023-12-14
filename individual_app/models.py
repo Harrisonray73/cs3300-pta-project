0930f52457cb138a20445acab51db0aa28d2166e
Harrisonray73@uccs.edu <107588181+Harrisonray73@users.noreply.github.com>
2023-12-13 21:58:50 -0700

    update9

--- individual_app/models.py
+++ individual_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+    title = models.CharField(max_length=200, default="title")
+    is_active = models.BooleanField(default = False)
+    about = models.TextField(blank = True)
+    contact_email = models.CharField(max_length=200, blank = True)
+
+
 class Member(models.Model):
 
 #List of choices for major value in database, human readable name
@@ -15,3 +22,4 @@ class Member(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField("USER Email", max_length=200)
     title = models.CharField(max_length=200, choices=TITLE, blank = False)
+    position = models.OneToOneField(Position, on_delete=models.CASCADE, unique=True)
--- individual_app/views.py
+++ individual_app/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views import generic
+from .models import Member, Position
 # Create your views here.
-def index(request):
+#def index(request):
+def member_view(request):
+    # Your view logic here
+    return render(request, 'individual_app/base_template.html')
+# Render index.html
+user_active_accounts = Member.objects.select_related('individual').all().filter(individual__is_active=True)
+print("active individual query set", user_active_accounts)
+#return render( request, 'individual_app/base_template.html')
 
-# Render the HTML template index.html with the data in the context variable.
-   return render( request, 'individual_app/member_template.html')
+class MemberListView(generic.ListView):
+    model = Member
 
+class MemberDetailView(generic.DetailView):
+    model = Member
+
+class PositionDetailView(generic.DetailView):
+    model = Position
